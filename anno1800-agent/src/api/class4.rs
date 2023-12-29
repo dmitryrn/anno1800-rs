8aa568c533e6ccbee4f1e46762e7ed7bda25e581
Benedikt Radtke <benediktradtke@gmail.com>
2023-12-29 23:04:48 +0100

    data data data

--- anno1800-agent/src/api/class4.rs
+++ anno1800-agent/src/api/class4.rs
@@ -9,26 +9,40 @@ impl Class4 {
         Self { address }
     }
 
-    pub fn get_efficiency_factor(&self) -> f32 {
-        unsafe { ((self.address + 0x01f0) as *const f32).read_volatile() }
+    pub fn get_16c(&self) -> u32 {
+        self.get(0x016c) // always 1?
     }
 
-    pub fn get_productivity(&self) -> u32 {
-        unsafe {((self.address + 0x01f4) as *const u32).read_volatile() }
+    pub fn get_type(&self) -> u32 {
+        self.get(0x0168)
+    }
+
+    pub fn get_potential_productivity_factor(&self) -> f32 {
+        self.get(0x01f0)
+    }
+
+    pub fn get_current_productivity_factor(&self) -> f32 {
+        self.get(0x01f4)
     }
 
     pub fn get_millis_per_cycle(&self) -> u64 {
-        unsafe {((self.address + 0x01f8) as *const u64).read_volatile() }
+        self.get(0x01f8)
+    }
+
+    fn get<T>(&self, offset: u64) -> T {
+        unsafe { ((self.address + offset) as *const T).read_volatile() }
     }
 }
 
 impl Debug for Class4 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Class4")
-            .field("address", &self.address)
-            .field("efficiency_factor", &self.get_efficiency_factor())
-            .field("productivity", &self.get_productivity())
-            .field("millis_per_cycle", &self.get_millis_per_cycle())
+            .field("address", &format!("{:#016x}", &self.address))
+            .field("field_16c", &format!("{:#08x}", &self.get_16c()))
+            .field("current_productivity_factor", &format!("{:.2}", &self.get_current_productivity_factor()))
+            .field("potential_productivity_factor", &format!("{:.2}", &self.get_potential_productivity_factor()))
+            .field("millis_per_cycle", &format!("{:05}", &self.get_millis_per_cycle()))
+            .field("type", &format!("{:#08x}", &self.get_type()))
             .finish()
     }
 }
